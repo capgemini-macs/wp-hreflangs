53a1cc20d69ac516fbcf351c3908ef694f09d024
Mirosław Mac <miroslaw.mac@capgemini.com>
2019-03-14 12:54:02 +0100

    added plugin.php

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: CapGemini Hreflangs
+ * Description: Creates hreflangs pointing to the same version of page in different languages
+ * Plugin URI: https://capgemini.com
+ * Author: Capgemini
+ * Author URI: https://capgemini.com
+ * License: GPLv2
+ *
+ * @package CapGemini
+ * @author  Mirosław Mac <miroslaw.mac@capgemini.com>
+ */
+
+namespace CG\Hreflangs;
+
+// Author override
+require_once __DIR__ . '/inc/namespace.php';
